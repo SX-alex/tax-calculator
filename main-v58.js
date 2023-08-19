d80ad090e7738ed133bbe58d43961dc74b0e65f4
SX-alex <140152415+SX-alex@users.noreply.github.com>
2023-08-19 19:50:12 +0300

    Update and rename main-v57.js to main-v58.js

--- main-v58.js
+++ main-v58.js
@@ -1438,6 +1438,7 @@ annualIncomeField.addEventListener('input', updateData);
 $('.form__get-tax').submit(()=>{
     loadingAllow = true;
     $('.trigger__show-tax-calc').click();
+    document.getElementById('results').style.dispa = flex;
     showTaxLoaderAndResults();
     updateData();
     setTimeout(() => {
